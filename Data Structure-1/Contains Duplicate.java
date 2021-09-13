133f0f4a110c824345bf0e67e037fc994124feae
Sunit Roy <2703iamsry@gmail.com>
2021-09-13 19:43:46 +0530

    Update Contains Duplicate.java

--- Data Structure-1/Contains Duplicate.java
+++ Data Structure-1/Contains Duplicate.java
@@ -1,1 +1,12 @@
+public boolean containsDuplicate(int[] nums) {
+    Set<Integer> set = new HashSet<>(nums.length);
+    for (int x: nums) {
+        if (set.contains(x)) return true;
+        set.add(x);
+    }
+    return false;
+}
 
+//Complexity Analysis  
+//Time complexity :  O ( n ) O(n). We do search() and insert() for  n n times and each operation takes constant time.  
+//Space complexity :  O ( n ) O(n). The space used by a hash table is linear with the number of elements in it.  
